fcc6dea1135880fbe176f862da4e86d532e339c9
Omegastick <isaac@poulton.dev>
2022-12-13 12:49:13 +0800

    Add remove non-images script

---
+++ remove_non_images.py
@@ -0,0 +1,37 @@
+"""CLI Utility that removes non-image files (with their metadata) from a directory."""
+
+import os
+from pathlib import Path
+
+import tqdm
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def remove_non_images(directory: Path = typer.Argument(..., exists=True, file_okay=False, dir_okay=True)) -> None:
+    """Removes non-image files from a directory recursively, prompting the user before deleting."""
+    files_to_delete: list[Path] = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = Path(root, file)
+            if all(
+                [
+                    suffix not in [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".json"]
+                    for suffix in file_path.suffixes
+                ]
+            ):
+                files_to_delete.append(file_path)
+
+    delete = typer.confirm(f"Delete {len(files_to_delete)} files?")
+    if not delete:
+        return
+
+    for file in tqdm.tqdm(files_to_delete):
+        os.remove(file)
+        os.remove(file.with_suffix(file.suffix + ".json"))
+
+
+if __name__ == "__main__":
+    app()
